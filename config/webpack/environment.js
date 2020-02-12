3fa5aeae07fc0aa911bebc1a5e38b4fc410855c2
venkat <venkat.bitla@gmail.com>
2020-02-12 19:13:53 +0530

    added bootstrap library

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,14 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
+const webpack = require('webpack')
 const vue = require('./loaders/vue')
-
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
